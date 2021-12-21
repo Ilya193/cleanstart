63b708e5ce9da4eacdec1144e52d020f5ca2ae13
ilya <ilya.komendantov@list.ru>
2021-12-21 16:41:59 +0300

    hilt

--- app/src/main/java/com/example/cleanstart/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/cleanstart/presentation/MainViewModel.kt
@@ -6,8 +6,11 @@ import androidx.lifecycle.ViewModel
 import com.example.cleanstart.domain.models.SaveUserNameParam
 import com.example.cleanstart.domain.usecase.GetUserNameUseCase
 import com.example.cleanstart.domain.usecase.SaveUserNameUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MainViewModel(
+@HiltViewModel
+class MainViewModel @Inject constructor(
     private val getUserNameUseCase: GetUserNameUseCase,
     private val saveUserNameUseCase: SaveUserNameUseCase,
 ) : ViewModel() {
